991fe9a3a786888925944f0c86b19058ce671707
devoidofgenius <cellinger@gmail.com>
2017-03-19 14:58:39 -0400

    I am not usually one for speeches. So, goodbye.

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+<<<<<<< HEAD
+import { render } from 'react-dom';
+import App from './components/App';
+
+render(<App />, document.getElementById('main'));
+=======
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+>>>>>>> 4091171... I am usually not one for speeches. So, goodbye.
